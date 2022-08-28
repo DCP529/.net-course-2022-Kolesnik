23f19c225ef41cdc4f0be17210f2872860e02f08
DCP529 <61003286+DCP529@users.noreply.github.com>
2022-08-29 01:10:33 +0300

    Update

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -4,11 +4,6 @@ namespace Models
 {
     public class Employee : Person
     {
-        public int Passport { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Patronymic { get; set; }
-        public DateTime BirthDate { get; set; }
         public string Contract { get; set; }
     }
 }
